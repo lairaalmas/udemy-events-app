b4ebe9d2ea5a1dec29eaa08261063108aa1584e5
lairaalmas <lairaalmas@gmail.com>
2023-03-02 20:54:51 -0300

    fetching data for page that depends on :id

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,7 +28,9 @@ import EventsRootLayout from "./pages/EventsRoot";
 import ErrorPage from "./pages/Error";
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 
@@ -48,7 +50,11 @@ const router = createBrowserRouter([
             element: <EventsPage />,
             loader: eventsLoader,
           },
-          { path: ":eventId", element: <EventDetailPage /> },
+          {
+            path: ":eventId",
+            element: <EventDetailPage />,
+            loader: eventDetailLoader,
+          },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
         ],
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,14 +1,27 @@
-import { useParams } from "react-router-dom";
+import EventItem from "../components/EventItem";
+
+import { useLoaderData, json } from "react-router-dom";
 
 const EventDetailPage = () => {
-  const params = useParams();
+  const data = useLoaderData();
 
-  return (
-    <>
-      <h1>Event detail page</h1>
-      <p>Event id: {params.eventId}</p>
-    </>
-  );
+  return <EventItem event={data.event} />;
 };
 
 export default EventDetailPage;
+
+// cannot use useParams hook, but receives from loader as props
+export const loader = async ({ request, params }) => {
+  const id = params.eventId;
+
+  const response = await fetch(`http://localhost:8080/events/${id}`);
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for selected event!" },
+      { status: 500 }
+    );
+  } else {
+    return response;
+  }
+};
