16648ce7ff3edbd49a5c9f72d0231113de84962d
lairaalmas <lairaalmas@gmail.com>
2023-03-03 01:30:41 -0300

    update homepage style

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,11 @@
+import PageContent from "../components/PageContent";
+
 const HomePage = () => {
-  return <h1>Home page</h1>;
+  return (
+    <PageContent title="Welcome!">
+      <p>Browse all our amazing events!</p>
+    </PageContent>
+  );
 };
 
 export default HomePage;
